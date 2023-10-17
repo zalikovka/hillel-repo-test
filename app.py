3ef82d3786c2310288a475be4095ab12d5ee9b93
Vitalii Pavliuk <pavliuk96@gmail.com>
2023-10-17 19:43:36 +0300

    add code which breaks

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
-print("Hello world!")
+
+def breaking_function():
+    1/0
+    print("This is a function that does nothing")
+
+
+def main():
+    breaking_function()
+
+
+if __name__ == "__main__":
+    main()
