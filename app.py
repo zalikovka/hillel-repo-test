80513bddd5b776857223f6d469019d099603a09a
Vitalii Pavliuk <pavliuk96@gmail.com>
2023-10-17 19:57:15 +0300

    start work on Windows

--- app.py
+++ app.py
@@ -1,13 +1,2 @@
-
-def non_breaking_function():
-    0/1
-    print("This is a function that does nothing")
-
-
-def main():
-    non_breaking_function()
-
-
-
-if __name__ == "__main__":
-    main()
+def windows():
+    return "Hello from Windows 10"
