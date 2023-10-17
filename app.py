a24b859fe85ceaf4f8441d7570a07bce045b5567
Vitalii Pavliuk <pavliuk96@gmail.com>
2023-10-17 20:43:00 +0300

    add windows 10.2 version

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
+# this is windows 10.2
 def windows():
-    print("💻💻💻")
-    return "Hello from Windows 10.1"
+    print("💻💻💻💻💻💻")
+    return "Hello from Windows 10.2"
 
 
 if __name__ == "__main__":
